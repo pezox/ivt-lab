00e0e6c09245fb4c4d94e4e3d3746aafdb421cdc
pezox <svpeti99@gmail.com>
2022-05-06 13:23:11 +0200

    GT4500Test implemented

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,16 +8,23 @@ import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
+  TorpedoStore primaryMock;
+  TorpedoStore secondaryMock;  
   private GT4500 ship;
 
   @BeforeEach
-  public void init(){
-    this.ship = new GT4500();
+  public void init() {
+    this.primaryMock = mock(TorpedoStore.class);
+    this.secondaryMock = mock(TorpedoStore.class);
+    this.ship = new GT4500(primaryMock, secondaryMock);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primaryMock.fire(1)).thenReturn(true);
+    when(primaryMock.isEmpty()).thenReturn(false);
+
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
@@ -29,6 +36,13 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(primaryMock.isEmpty()).thenReturn(false);
+    when(primaryMock.getTorpedoCount()).thenReturn(10);
+    when(primaryMock.fire(10)).thenReturn(true); 
+    
+    when(secondaryMock.isEmpty()).thenReturn(false);
+    when(secondaryMock.getTorpedoCount()).thenReturn(10);
+    when(secondaryMock.fire(10)).thenReturn(true);     
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
